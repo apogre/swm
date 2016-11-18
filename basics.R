bf9d1468a2fec5bb086b66cc0a1837a8aac0cb1c
anishpradhan <mail.anishpradhan@gmail.com>
2016-11-18 00:01:44 -0700

    kmean

--- basics.R
+++ basics.R
@@ -1,7 +1,44 @@
+library(rCharts)
+library(devtools)
+install_github('rCharts','ramnathv')
+install.packages(c("devtools", "RCurl", "rjson", "bit64","httr","ROAuth"))
+install_github('rCharts', 'ramnathv')
+
 getwd()
 setwd("/home/apradhan/proj/swm")
-data = read.csv("follow.csv",header = TRUE)
+data = read.csv("full_data1.csv",header = TRUE)
+summary(data)
+data[data==0]<-1
 
-plot(data$friends_count,data$followers_count)
+
+data$log_friends_count<-log(data$friends_count)
+data$log_followers_count<-log(data$followers_count)
+
+
+summary(data)
+
+log_data <- data.frame(data$log_friends_count,data$log_followers_count)
+
+wss <- (nrow(log_data)-1)*sum(apply(log_data,2,var))
+for (i in 2:15) wss[i] <- sum(kmeans(log_data,centers=i)$withinss)
+
+plot(1:15, wss, type="b", xlab="Number of Clusters",ylab="Within groups sum of squares")
+
+userMeans.log <- kmeans(log_data, centers=5, iter.max=10, nstart=100)
+log_data$cluster=factor(userMeans.log$cluster)
+data$cluster <-log_data$cluster
+
+
+write.csv(data, file = "cluster_lfc_5.csv", row.names = FALSE)
 
 head(data)
+
+newdata <- data[ which(data$followers_count<10),]
+
+data1<-data
+data1$ratio = data1$friends_count/data1$followers_count
+
+attach(newdata)
+plot(friends_count,followers_count)
+
+detach(newdata)
--- data_extractor.py
+++ data_extractor.py
@@ -7,3 +7,6 @@ collection = conn['db']['user_info']
 for doc in json.load(open('users.json')):
     new_doc = {k: v for k, v in doc.iteritems() if k in ('email', 'username', 'avatar')}
     collection.update({'_id': doc['_id']}, {'$set': new_doc})
+
+
+  mongoexport -h localhost -d local -c user_info --type=csv -f "id,screen_name,name,created_at,time_zone,lang,location,friends_count,description,statuses_count,verified,followers_count,favourites_count" -o full_data1.csv
